2c018837fd4a82f58309af9c88517532d056621a
Miles Zimmerman <miles@zserver.org>
2014-09-06 22:55:16 -0700

    Fixes to CLI

--- bin/tera.js
+++ bin/tera.js
@@ -10,8 +10,15 @@ var program = require('commander');
  */
 program
   .version(package.version, '-v, --version')
-  .command('new [app]', 'Generate a new Tera app')
+  .usage('<command> [options]')
+  .command('new [app]', 'Create a new Tera app in the current directory')
   .command('generate', 'Generate a model, controller, listener, resource, or migration')
   .command('destroy', 'Destroy a model, controller, listener, resource, or migration')
   .command('server', 'Start the node server (you can also run `node index.js`)')
   .parse(process.argv);
+
+
+// Print help if nil
+program
+  .command('*')
+  .action(program.help());
